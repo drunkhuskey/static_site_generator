422439ca8fd922581cad75ba3894ac821cea3aff
drunkhuskey <sam.orent@gmail.com>
2025-05-18 22:47:43 -0400

    added parent html nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,6 +18,24 @@ class HTMLNode:
             prop_string += f" {key}=\"{self.props[key]}\""
         return prop_string
     
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+        
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("no tag assigned")
+        if self.children == None:
+            raise ValueError("no children assigned")
+        text = f"<{self.tag}"
+        if self.props != None:
+            text += self.props_to_html()
+        text += ">"
+        for child in self.children:
+            text += child.to_html()
+        text += f"</{self.tag}>"
+        return text
+    
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -28,6 +28,23 @@ class TestTextNode(unittest.TestCase):
     def test_leaf_to_html_b(self):
         node = LeafNode("b", "Hello, world!")
         self.assertEqual(node.to_html(), "<b>Hello, world!</b>")
+        
+    def test_empty_parent_node(self):
+        parent_node = ParentNode("div", [])
+        self.assertEqual(parent_node.to_html(), "<div></div>")
+        
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",)
 
 
 if __name__ == "__main__":
